4e7492a0364ec3a5944d44c29059e3c850589336
aniskin.a.a <andrei.aniskin@yandex.ru>
2022-07-27 01:27:52 +0300

    1st commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<a href="https://codeclimate.com/github/BlackInBlack/java-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/b6590b41d8abebaf825d/maintainability" /></a>
+
+
+Even:
+https://asciinema.org/a/11q0UMbHx91mJROIO3WGP0gPo
+
+Calc:
+https://asciinema.org/a/I8vmunzXGYsvU3878xCc1nhZ8
+
+GCD:
+https://asciinema.org/a/vt8QBRRdaCb7gltpxFCs771P1
+
+Progression:
+https://asciinema.org/a/1N3hIcgoHx5H6Jv6piFAVdcP3
+
+Prime:
+https://asciinema.org/a/zYF8G9UiIAIDvomMoIEEBh7yi
+
+
---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static String getUserName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        return sc.nextLine();
+    }
+
+    public static int getGameNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "2 - Even\n" +
+                "3 - Calc\n" +
+                "4 - GCD\n" +
+                "5 - Progression\n" +
+                "6 - Prime\n" +
+                "0 - Exit\n" +
+                "Your choice: ");
+        return sc.nextInt();
+    }
+
+    public static String getUserAnswer() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+}
